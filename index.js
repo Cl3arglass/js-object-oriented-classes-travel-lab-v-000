ac2e4f4366d720223f610d6bfbd9305dedcf75b9
Holden Altaffer <haltaffer@gmail.com>
2018-12-17 04:14:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    let beg = new Date(year, 1).getFullYear();
+    return beg - this.startDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+     this.beginningLocation = beginningLocation;
+     this.endingLocation = endingLocation;
+   }
+}
